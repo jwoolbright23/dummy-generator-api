3d17626ced983b75dde20f59325a5ce53108d5d8
John Woolbright <jwoolbright23@gmail.com>
2022-03-24 15:54:32 -0500

    Updated imports, added variable for filepaths within each endpoint call

--- main.py
+++ main.py
@@ -1,80 +1,62 @@
-# Need multiple get requests
-
-# Create new data and return it
-
-# End point for dummy data we created: User data, sensitive user data, bank account data, ip data
-
-'''
-- GET /data/user -> CSV File
-- GET /data/user/sensitive
-- GET /data/transaction
-- GET /data/ip-logs
-'''
-
+from csv import DictReader
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-import csv
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"csv": "John Matthews, superUser, pw: safelyregistered, email: whoknows@email.com"}
-
-'''
-GET /data/user -> CSV file
-GET /data/user?type=json -> JSON representation of the CSV file
-'''
 def unpack_csv_file(csv_filepath):
     data = []
     with open(csv_filepath, 'r') as csvfile:
-        some_reader = csv.DictReader(csvfile)
+        some_reader = DictReader(csvfile)
         for row in some_reader:
             data.append(row)
     return data
 
 @app.get("/data/user")
 async def get_data_user(data_format="json"):
+    filepath = "dummy-user-data1.csv"
     # guard clause 1 (provided query parameter data_format=csv)
     if data_format == "csv":
-        return FileResponse("dummy-user-data1.csv", filename="random-user-data.csv")
+        return FileResponse(filepath, filename="random-user-data.csv")
     
     # default behavior of endpoint (no options: query parameters, path variable, request body)
-    json_data = unpack_csv_file("dummy-user-data1.csv")
+    json_data = unpack_csv_file(filepath)
 
     return json_data
 
 @app.get("/data/user/sensitive")
 async def get_sensitive_user_data(data_format="json"):
+    filepath = "dummy-sensitive-user-data.csv"
     # guard clause 1 (provided query parameter data_format=csv)
     if data_format == "csv":
-        return FileResponse("dummy-sensitive-user-data.csv", filename="random-sensitive-user-data.csv")
+        return FileResponse(filepath, filename="random-sensitive-user-data.csv")
     
     # default behavior of endpoint (no options: query parameters, path variable, request body)
-    json_data = unpack_csv_file("dummy-sensitive-user-data.csv")
+    json_data = unpack_csv_file(filepath)
 
     return json_data
 
 @app.get("/data/transaction")
 async def get_transaction_data(data_format="json"):
+    filepath = "dummy-bank-account-data.csv"
     # guard clause 1 (provided query parameter data_format=csv)
     if data_format == "csv":
-        return FileResponse("dummy-bank-account-data.csv", filename="random-bank-account-data.csv")
+        return FileResponse(filepath, filename="random-bank-account-data.csv")
     
     # default behavior of endpoint (no options: query parameters, path variable, request body)
-    json_data = unpack_csv_file("dummy-bank-account-data.csv")
+    json_data = unpack_csv_file(filepath)
 
     return json_data
 
 @app.get("/data/ip-logs")
 async def get_ip_data(data_format="json"):
+    filepath = "dummy-ip-data.csv"
     # guard clause 1 (provided query parameter data_format=csv)
     if data_format == "csv":
-        return FileResponse("dummy-ip-data.csv", filename="random-ip-data.csv")
+        return FileResponse(filepath, filename="random-ip-data.csv")
     
     # default behavior of endpoint (no options: query parameters, path variable, request body)
-    json_data = unpack_csv_file("dummy-ip-data.csv")
+    json_data = unpack_csv_file(filepath)
 
     return json_data
