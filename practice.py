383493e5c8a71c6e2124a612409a03d01a5ffdc0
John Woolbright <jwoolbright23@gmail.com>
2022-03-29 14:25:20 -0500

    added routers folder, added __init__.py to root folder, added walkthrough.py for walkthrough files

---
+++ practice.py
@@ -0,0 +1,8 @@
+import lc_pydatagenerator.generate_random
+
+from lc_pydatagenerator.generate_random import user_data, sensitive_user_data, transaction_data, ip_data
+
+user_data()
+sensitive_user_data()
+ip_data()
+transaction_data()
---
+++ routers/walkthrough.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter
+
+from fastapi.responses import FileResponse
+
+from csv import DictReader
+
+router = APIRouter(prefix="/api/walkthrough")
+
+def unpack_csv_file(csv_filepath):
+    data = []
+    with open(csv_filepath, 'r') as csvfile:
+        some_reader = DictReader(csvfile)
+        for row in some_reader:
+            data.append(row)
+    return data
+
+@router.get("/user")
+async def get_data_user(data_format="json"):
+    filepath = "csvs/walkthrough/dummy-user-data.csv"
+    # guard clause 1 (provided query parameter data_format=csv)
+    if data_format == "csv":
+        return FileResponse(filepath, filename="random-user-data.csv")
+    
+    # default behavior of endpoint (no options: query parameters, path variable, request body)
+    json_data = unpack_csv_file(filepath)
+
+    return json_data
+
+@router.get("/sensitive")
+async def get_sensitive_user_data(data_format="json"):
+    filepath = "csvs/walkthrough/dummy-sensitive-user-data.csv"
+    # guard clause 1 (provided query parameter data_format=csv)
+    if data_format == "csv":
+        return FileResponse(filepath, filename="random-sensitive-user-data.csv")
+    
+    # default behavior of endpoint (no options: query parameters, path variable, request body)
+    json_data = unpack_csv_file(filepath)
+
+    return json_data
+
+@router.get("/transaction")
+async def get_transaction_data(data_format="json"):
+    filepath = "csvs/walkthrough/dummy-transaction-data.csv"
+    # guard clause 1 (provided query parameter data_format=csv)
+    if data_format == "csv":
+        return FileResponse(filepath, filename="random-bank-account-data.csv")
+    
+    # default behavior of endpoint (no options: query parameters, path variable, request body)
+    json_data = unpack_csv_file(filepath)
+
+    return json_data
+
+@router.get("/ip-logs")
+async def get_ip_data(data_format="json"):
+    filepath = "csvs/walkthrough/dummy-ip-data.csv"
+    # guard clause 1 (provided query parameter data_format=csv)
+    if data_format == "csv":
+        return FileResponse(filepath, filename="random-ip-data.csv")
+    
+    # default behavior of endpoint (no options: query parameters, path variable, request body)
+    json_data = unpack_csv_file(filepath)
+
+    return json_data
