591d12bc59a18ca2820af0c62d2f333751343459
John Woolbright <jwoolbright23@gmail.com>
2022-03-29 15:13:37 -0500

    added random router

--- routers/random.py
+++ routers/random.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter
 
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 
 from csv import DictReader
 
 from lc_pydatagenerator.generate_random import user_data
 
+from os import remove
+
+from starlette.background import BackgroundTask
+
 router = APIRouter(prefix="/api/random")
+
+def remove_file(path):
+    remove(path)
 
 def unpack_csv_file(csv_filepath):
     data = []
@@ -15,18 +22,21 @@ def unpack_csv_file(csv_filepath):
         for row in some_reader:
             data.append(row)
     return data
+
 @router.get("/user")
 async def get_data_user(data_format="json"):
 
     filepath = "csvs/random/random-dummy-user-data.csv"
 
     user_data(filepath, 1000)
+    
+    task = BackgroundTask(remove_file, path=filepath)
 
     # guard clause 1 (provided query parameter data_format=csv)
     if data_format == "csv":
-        return FileResponse(filepath, filename="random-user-data.csv")
+        return FileResponse(filepath, filename="random-user-data.csv", background=task)
     
     # default behavior of endpoint (no options: query parameters, path variable, request body)
     json_data = unpack_csv_file(filepath)
 
-    return json_data
+    return JSONResponse(content=json_data, background = task)
