79f6051a544742acd11bfa6e69c4c1f60ffdbf6c
John Woolbright <jwoolbright23@gmail.com>
2022-03-29 14:33:14 -0500

    added exercise.py file for exercise portion of user-applications chapter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from routers import walkthrough
+from routers import exercise
 
 app = FastAPI()
 
 app.include_router(walkthrough.router)
+app.include_router(exercise.router)
